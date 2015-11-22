2dd27234f81fa61c94edf102c3afa0f49834d6e4
Pablo Ortega Mesa <pablo.ortega.mesa@gmail.com>
2015-11-22 18:27:49 -0300

    first commit

---
+++ CoffeeKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoffeeKit
+//
+//  Created by Pablo Ortega Mesa on 22-11-15.
+//  Copyright © 2015 khrno.cl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoffeeKit/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  CoffeeKit
+//
+//  Created by Pablo Ortega Mesa on 22-11-15.
+//  Copyright © 2015 khrno.cl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
